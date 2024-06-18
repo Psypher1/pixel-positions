4f2b50c507dc0155304f93bb4c93f5184930e54f
dante <jamesmidzi@gmail.com>
2024-06-18 15:44:51 +0200

    chore: update views

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,16 +4,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Hanken+Grotesk:wght@100..900&display=swap" rel="stylesheet">
 
     <title>Pixel Positions</title>
+    @vite(['resources/js/app.js', 'resources/css/app.css'])
 </head>
 
-<body>
+<body class='font-hanken'>
     <header>
         <nav>
             <div>
                 <a href="/">
-                    {{-- <img src="{{ Vite::asste('resources/images/logo.svg') }}" alt=""> --}}
+                    <img src="{{ Vite::asset('resources/images/logo.svg') }}" alt="">
                     Pixel Positions
                 </a>
             </div>
