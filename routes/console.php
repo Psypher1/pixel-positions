e35576508d8212efb931538f7905fca0a48467be
dante <jamesmidzi@gmail.com>
2024-06-18 16:47:31 +0200

    chore: modified to try fix termnal

--- routes/console.php
+++ routes/console.php
@@ -3,6 +3,23 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
+/*
+|--------------------------------------------------------------------------
+| Console Routes
+|--------------------------------------------------------------------------
+|
+| This file is where you may define all of your Closure based console
+| commands. Each Closure is bound to a command instance allowing a
+| simple approach to interacting with each command's IO methods.
+|
+*/
+
+// Artisan::starting(function ($artisan) {
+//     $artisan->resolveCommands([
+//         \App\Console\Commands\CustomServeCommand::class,
+//     ]);
+// });
+
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
