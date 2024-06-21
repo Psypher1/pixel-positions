0b66c39b9f9771d33d664d8522b466b7e830c6af
dante <jamesmidzi@gmail.com>
2024-06-21 21:47:03 +0200

    feat: setup factories

---
+++ database/factories/JobFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Employer;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Job>
+ */
+class JobFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->jobTitle,
+            'salary' => fake()->randomElement(['$50 000', '$100 000', '$150 000', '$200 000']),
+            'location' => 'Remote',
+            'job_type' => fake()->randomElement(['Full Time', 'Part Time', 'Contract']),
+            'url' => fake()->url,
+            'featured' => false,
+            'employer_id' => Employer::factory(),
+
+
+        ];
+    }
+}
