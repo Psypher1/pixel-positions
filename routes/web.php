89d57af9a23f7dd46505b6971a2c7752a52c14ca
dante <jamesmidzi@gmail.com>
2024-06-28 17:09:24 +0200

    feat: start auth processes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\RegisterUserController;
+use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [JobController::class, 'index']);
+
+
+// AUTH ROUTEs
+Route::get('/auth/register', [RegisterUserController::class, 'create']);
+Route::post('/auth/register', [RegisterUserController::class, 'store']);
+
+Route::get('/auth/login', [SessionController::class, 'create']);
+Route::get('/auth/login', [SessionController::class, 'store']);
+Route::get('/auth/logout', [SessionController::class, 'destroy']);
