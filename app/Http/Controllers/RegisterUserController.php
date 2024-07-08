0c71d3186352b763897b96100c384692122fa78d
dante <jamesmidzi@gmail.com>
2024-07-08 10:59:49 +0200

    chore: clean up controller

--- app/Http/Controllers/RegisterUserController.php
+++ app/Http/Controllers/RegisterUserController.php
@@ -10,13 +10,6 @@ use Illuminate\Validation\Rules\Password;
 
 class RegisterUserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
 
     /**
      * Show the form for creating a new resource.
@@ -60,35 +53,5 @@ class RegisterUserController extends Controller
 
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
 }
