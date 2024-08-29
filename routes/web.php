7cf0be29d07149f5c37072cba8a4f4a8a19e35a9
dante <jamesmidzi@gmail.com>
2024-08-29 14:24:46 +0200

    fix: straggling files

--- routes/web.php
+++ routes/web.php
@@ -9,11 +9,25 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [JobController::class, 'index']);
+Route::get('/jobs/{job}', [JobController::class, 'show']);
 
-Route::middleware('auth')->group(function () {
-    Route::get('/jobs/create', [JobController::class, 'create']);
+Route::middleware(['auth'])->group(function () {
+    Route::get('/jobs/create', [JobController::class, 'create'])->name('jobs.create');
     Route::post('/jobs', [JobController::class, 'store']);
+
+    Route::get('/jobs/{job}/edit', [JobController::class, 'edit']);
+
 });
+
+// Route::controller(JobController::class)->group(function () {
+//     Route::get('/', 'index');
+//     Route::get('/jobs/{job}', 'show');
+
+//     Route::get('/jobs/create', 'create')->middleware('auth')->name('jobs.create');
+//     Route::post('/jobs', 'store')->middleware('auth');
+//     Route::get('/jobs/{job}/edit', 'edit')->middleware('auth');
+
+// });
 
 // invokable controller
 Route::get('/search', SearchController::class);
@@ -31,4 +45,6 @@ Route::middleware('guest')->group(function () {
 });
 
 Route::delete('/auth/logout', [SessionController::class, 'destroy'])->middleware('auth');
+
+
 
