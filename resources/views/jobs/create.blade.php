61d745096b21e82f238fcb0009b80a2a6a9fe16d
dante <jamesmidzi@gmail.com>
2024-09-10 13:57:44 +0200

    revert create page

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -1,3 +1,31 @@
 <x-layout>
-    <p>create</p>
+    <section class="pt-6">
+        <x-page-heading>Post A Job</x-page-heading>
+
+        <x-forms.form method="POST" action="/jobs">
+            <x-forms.input label="Title" name="title" placeholder="Title" />
+            <x-forms.input label="Salary" name="salary" placeholder="$200 000" />
+
+            {{-- <x-forms.input label="Location" name="location" placeholder="Location" /> --}}
+
+            <x-forms.select label="Location" name="location">
+                <option>On Site</option>
+                <option>Remote</option>
+                <option>Hybrid</option>
+            </x-forms.select>
+            <x-forms.select label="Job Type" name="job_type">
+                <option>Full Time</option>
+                <option>Part Time</option>
+                <option>Contract</option>
+            </x-forms.select>
+
+            <x-forms.input label="URL" name="url" placeholder="https://keystoneinc.com" />
+            <x-forms.checkbox label="Featured (Costs Extra)" name="featured" />
+
+            <x-forms.divider />
+            <x-forms.input label="Tags (comma separated)" name="tags" placeholder="admin, frontend, marketing" />
+
+            <x-forms.button>Publish</x-forms.button>
+        </x-forms.form>
+    </section>
 </x-layout>
